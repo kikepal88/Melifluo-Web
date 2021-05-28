8513fa491b81686b5d65dfd6dc39369f1e890721
Omar Palacios <omarpal88@gmail.com>
2021-05-27 21:53:08 -0500

    Se crean atributos en el HTML necesarios para mostrar y ocultar los contenedores de clases, de igual manera los estilos y la logica para el mismo proposito

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const idModulesArray = [];
 const modulesByClass = Array.from(document.getElementsByClassName("module__img"));
 console.log(modulesByClass);
+const idClassContainerArray = [];
+const classByClassName = Array.from(document.getElementsByClassName("module__class-conteiner"));
+console.log(classByClassName);
 
 function getIdModules() {
   modulesByClass.forEach(function(item){
@@ -12,6 +15,17 @@ function getIdModules() {
 getIdModules();
 console.log(idModulesArray);
 
+function getIdClass() {
+  classByClassName.forEach(function(item){
+    let id = item.getAttribute("id");
+    idClassContainerArray.push(id);
+  })
+};
+
+getIdClass();
+console.log(idClassContainerArray);
+
 function desplegarModulo(index) {
-document.getElementById(`Module${index + 1}`).classList.toggle("is-active");
+  document.getElementById(`Module${index + 1}`).classList.toggle("is-active");
+  document.getElementById(`class-conteiner${index + 1}`).classList.toggle("is-active");
 }
