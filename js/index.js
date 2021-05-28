34e18231bcc4b4887cc85996477b683f07ac6f9b
Omar Palacios <omarpal88@gmail.com>
2021-05-27 22:14:05 -0500

    Se crean atributos en el HTML necesarios para mostrar y ocultar los contenedores de las respuestas y cambias de iconos, de igual manera los estilos y la logica para el mismo proposito

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 const idModulesArray = [];
 const modulesByClass = Array.from(document.getElementsByClassName("module__img"));
-console.log(modulesByClass);
 const idClassContainerArray = [];
 const classByClassName = Array.from(document.getElementsByClassName("module__class-conteiner"));
-console.log(classByClassName);
+const idFaqIconArray = [];
+const faqIconByClass = Array.from(document.getElementsByClassName("faq__icon"));
+const idFaqAnswerArray = [];
+const faqAnswerByClass = Array.from(document.getElementsByClassName("faq__answer"));
 
 function getIdModules() {
   modulesByClass.forEach(function(item){
@@ -13,7 +15,6 @@ function getIdModules() {
 };
 
 getIdModules();
-console.log(idModulesArray);
 
 function getIdClass() {
   classByClassName.forEach(function(item){
@@ -23,9 +24,31 @@ function getIdClass() {
 };
 
 getIdClass();
-console.log(idClassContainerArray);
+
+function getIdFaqIcon() {
+  faqIconByClass.forEach(function(item){
+    let id = item.getAttribute("id");
+    idFaqIconArray.push(id);
+  })
+};
+
+getIdFaqIcon();
+
+function getIdFaqAnswer() {
+  faqAnswerByClass.forEach(function(item){
+    let id = item.getAttribute("id");
+    idFaqAnswerArray.push(id);
+  })
+};
+
+getIdFaqAnswer();
 
 function desplegarModulo(index) {
   document.getElementById(`Module${index + 1}`).classList.toggle("is-active");
   document.getElementById(`class-conteiner${index + 1}`).classList.toggle("is-active");
+}
+
+function desplegarRespuesta(index) {
+  document.getElementById(`faq-icon${index + 1}`).classList.toggle("is-active");
+  document.getElementById(`faq-answer${index + 1}`).classList.toggle("is-active");
 }
